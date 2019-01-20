cf89bb264db503537b86d5b0c6e670c0dda9fdc5
orgrinrt <ollipekka.toimela@gmail.com>
2019-01-20 05:51:21 +0200

    Renamed ResourceTypes addon to Talesmith

--- Addons/ResourceTypes/app_prefs.gd
+++
@@ -1,4 +0,0 @@
-extends Resource
-
-export var app_version : float = 0.1;
-export var ui_animation_speed : float = 0.5;
--- Addons/ResourceTypes/view_prefs.gd
+++
@@ -1,4 +0,0 @@
-extends Resource
-
-export var show_inspector : bool = true;
-export var use_big_icons : bool = false;
