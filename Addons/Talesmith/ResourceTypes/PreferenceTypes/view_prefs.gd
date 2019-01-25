f2892778130d3b892761876f00dca1e0b0c165f6
orgrinrt <ollipekka.toimela@gmail.com>
2019-01-25 09:27:32 +0200

    -Renamed Addons folder => addons (for compatibility)

--- Addons/Talesmith/ResourceTypes/PreferenceTypes/view_prefs.gd
+++ Addons/Talesmith/ResourceTypes/PreferenceTypes/view_prefs.gd
@@ -2,4 +2,5 @@ extends Resource
 
 export(bool) var show_inspector : bool = true;
 export(bool) var show_binder : bool = true;
+export(bool) var show_dock : bool = true;
 export(bool) var use_big_icons : bool = false;
